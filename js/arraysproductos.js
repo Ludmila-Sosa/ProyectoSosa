c51fbac57f3b2e822bd3731c086a72f09160f2b6
Ludmila Sosa <ludmil4sos4@gmail.com>
2022-08-11 09:35:47 -0300

    Agregado de contenido y arrays

--- js/arraysproductos.js
+++ js/arraysproductos.js
@@ -8,45 +8,36 @@ function ingresarNombre() {
     }
 }
 
-class Producto {
-    constructor(id, nombre, importe) {
-        this.id = id
-        this.nombre = nombre
-        this.importe = importe
-    }
-    precioFinal() {
-        return parseFloat((this.importe * IVA).toFixed(2))
-    }
-}
-
-function creoID(){return parseInt(Math.random()*10000)}
-
-const productos =[]
-const IVA = 1.21
-
-function agregoProductos(){
-    let id = creoID()
-    let descripcion = prompt("Ingresa el nombre del producto:")
-    let precio = parseInt(prompt("Ingrese importe:"))
-        productos.push(new Producto(id, descripcion, precio))
-        console.table(productos)
-}
-
-function listaProductos(){
-    productos.push(new Producto(0567, "PELUCHES SKZOO", 2000))
-    productos.push(new Producto(1387, "BUZO STRAY KIDS", 5000))
-    productos.push(new Producto(2987,"POSTERS", 500))
-    productos.push(new Producto(3485, "ÁLBUM ODDINARY", 15000))
-    productos.push(new Producto(4587,"ÁLBUM NOEASY", 10000))
-    productos.push(new Producto(5837,"REMERA PERSONALIZADA SKZ",2500))
-    console.table(productos)
-}
-
-function recorrerProductos(){
-    debugger
-    for (let producto of productos){
-        console.table(producto)
-    }
-}
-
 ingresarNombre()
+
+let stockProductos= [
+    {id:1, nombre: "Peluches SKZOO", desc: "Tierno peluche SKZOO", precio: 3500},
+    {id:2, nombre: "Buzo Stray Kids", desc: "Buzo estampado", precio: 5000},
+    {id:3, nombre: "Álbum ODDINARY", desc: "Incluye photocards y posters", precio: 13500},
+    {id:4, nombre: "Álbum MIXTAPE", des: "Discografía oficial de Stray Kids", precio: 10000},
+    {id:5, nombre: "Remera BangChan", desc: "Remera excelente calidad de BCH97", precio: 5500},
+    {id:6, nombre:"Posters y Photocards", desc: "Posters y photocards", precio: 600},
+]
+
+console.table(stockProductos)
+
+let carritoDeCompras = []
+
+agregarAlCarrito()
+
+function agregarAlCarrito() {
+    let elijoProducto = parseInt(prompt('Ingrese el ID de el producto que desee comprar')) 
+
+    let productoAgregar = stockProductos.find((el) => el.id == elijoProducto)
+    carritoDeCompras.push(productoAgregar)
+     console.log(carritoDeCompras)
+    actualizarCarrito()
+}
+
+function actualizarCarrito() {
+    console.log('Cantidad de productos agregados ' + carritoDeCompras.length)
+    let suma = carritoDeCompras.reduce((acc, el) => acc + el.precio, 0)
+
+    console.log('La suma total de tu carrito es $' + suma)
+}
+
